5c786bfed7bf81a50f9d43cc0e64ce45494c1df4
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-02-25 13:59:37 +0900

    Lv.5 클라이언트에게 1이상의 숫자를 입력하도록 검증로직 추가

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.todo.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
@@ -10,8 +11,10 @@ import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
 import org.example.expert.domain.todo.service.TodoService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 public class TodoController {
@@ -26,10 +29,12 @@ public class TodoController {
         return ResponseEntity.ok(todoService.saveTodo(authUser, todoSaveRequest));
     }
 
+    //1. 개떡같이 입력해도 찰떡같이 알아들는 서버가될것이냐 Math.max()
+    //2. 명확한기준으로 클라이언트로부터 올바른 입력을 이끌어내는 서버가 될것이냐 @Validated
     @GetMapping("/todos")
     public ResponseEntity<Page<TodoResponse>> getTodos(
-            @RequestParam(defaultValue = "1") int page,
-            @RequestParam(defaultValue = "10") int size
+            @RequestParam(defaultValue = "1") @Positive  int page,
+            @RequestParam(defaultValue = "10") @Positive int size
     ) {
         return ResponseEntity.ok(todoService.getTodos(page, size));
     }
